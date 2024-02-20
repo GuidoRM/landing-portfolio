f59981721e0c606e659242c8b3e31671422229c5
GuidoRM <guidorafaelmamani100@gmail.com>
2024-02-20 02:39:53 -0300

    fear: hero section design for mobile

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -2,38 +2,54 @@ import Image from 'next/image';
 import React from 'react';
 import Programador from '@/public/programador.png';
 import Badge from '@/public/badge.png';
-import Link from 'next/link';
 
 const Hero = () => {
   return (
-    <section>
-      <section>
-        <h1>
-          Hey There,<br/>
-          I&apos;m Jonatan
-        </h1>
+    <section className='container-fluid hero'>
 
-        <a 
-          href="mailto:guidorafaelmamani100@gmail.com" 
-          target="_blank" rel="noopener noreferrer">
-          guidorafaelmamani100@gmail.com
-        </a>
+      <div className='row'>
 
-        <p>
-          <strong>3</strong>
-          YEARS EXPERIENCES
-        </p>
-      </section>
-      <Image src={Programador} alt='Imagen personal' className=''/>
+        <section className='col-lg-6'>
+          <h1 className='hero__title'>
+            Hey There,<br />
+            I&apos;m Jonatan
+          </h1>
 
-      <section>
-        <p>I design beautifully simple things, And I love what i do</p>
+          <a
+            href="mailto:guidorafaelmamani100@gmail.com"
+            target="_blank" rel="noopener noreferrer"
+            className='hero__email d-none d-md-block'
+          >
+            guidorafaelmamani100@gmail.com
+          </a>
 
-        <section>
-          <Image src={Badge} alt='Badged Full Stack Developer' className=''/>
-          <h3>FullStack JavaScript Developer</h3>
+          <p className='hero__experiencies'>
+            <strong>3 </strong>
+            YEARS EXPERIENCES
+          </p>
+
+          <p className='hero__subtitle d-md-none'>I design beautifully simple things, And I love what i do</p>
+
+
         </section>
-      </section>
+
+        <section className='col-lg-6 hero__badge d-none d-md-block'>
+
+          <p>I design beautifully simple things, And I love what i do</p>
+
+          <section>
+            <Image src={Badge} alt='Badged Full Stack Developer' />
+            <h3>FullStack JavaScript Developer</h3>
+          </section>
+
+        </section>
+
+        <section className='hero__images container-fluid text-center'>
+          <Image src={Programador} alt='Imagen personal' className='hero__images-photo'/>
+          <Image src={Badge} alt='Badged Full Stack Developer' className='d-md-none hero__images-badge' />
+        </section>
+
+      </div>
 
     </section>
   )
