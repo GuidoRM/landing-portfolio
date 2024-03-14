2538a0162fa04f9f20308ab6be6fa49c49059e4d
GuidoRM <guidorafaelmamani100@gmail.com>
2024-03-13 22:38:29 -0300

    feat: Footer Section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,69 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
+
+  const TITLE = "</Guido Dev>";
+
+  const BUTTONS = [
+    {
+      name: "SERVICES",
+      href: ""
+    },
+    {
+      name: "PROJECTS",
+      href: ""
+    },
+    {
+      name: "NOTES",
+      href: ""
+    },
+    {
+      name: "EXPERIENCES",
+      href: ""
+    }
+  ];
+
+
   return (
-    <div>Footer</div>
-  )
+    <footer className='footer container-fluid'>
+
+      <section className="footer__section row">
+
+        <div className='col-12 col-md-8 footer__subsectionLeft'>
+          <h3>Let’s Make Something Amazing Together</h3>
+          <h3>Start by <strong>saying hi</strong></h3>
+        </div>
+
+        <div className='col-12 col-md-4 footer__subsectionRight'>
+          <div>
+            <h3>Information</h3>
+            <p>Cancún, Quintana Roo. Mex.</p>
+          </div>
+
+          <ul>
+            {
+              BUTTONS.map((button, index) => (
+                <li key={`navbarButton-${index}`} className='navbar__buttons-option'>
+                  <Link href={button.href}>
+                    {button.name}
+                  </Link>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+
+      </section>
+
+      <hr />
+
+      <section className='footer__reminders'>
+        <h3 className='d-none d-md-block'>{TITLE}</h3>
+        <p>Powered By <strong>DevKoore</strong> con 💝</p>
+      </section>
+    </footer>
+  );
 }
 
 export default Footer;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,45 +1,86 @@
+import Link from 'next/link';
 import React from 'react';
+import WhatsappSVG from './Home/WhatsappSVG';
 
 const Nav = () => {
+
+  const TITLE = "</Joni Dev>";
+
+  const BUTTONS = [
+    {
+      name: "SERVICES",
+      href: ""
+    },
+    {
+      name: "PROJECTS",
+      href: ""
+    },
+    {
+      name: "NOTES",
+      href: ""
+    },
+    {
+      name: "EXPERIENCES",
+      href: ""
+    }
+  ];
+
   return (
 
-    <nav class="navbar bg-body-tertiary fixed-top">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Offcanvas navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar fixed-top">
+
+      <div className="container-fluid navbarWrapper">
+
+        <a className="navbar-brand navbar__title" href="#">{TITLE}</a>
+
+        <ul className='navbar__buttons d-none d-xl-flex'>
+          {
+            BUTTONS.map((button, index) => (
+              <li key={`navbarButton-${index}`} className='navbar__buttons-option'>
+                <Link href={button.href}>
+                  {button.name}
+                </Link>
+              </li>
+            ))
+          }
+
+          <li className='navbar__buttons-whatsapp'>
+            +54 2964-476955
+            <WhatsappSVG />
+          </li>
+        </ul>
+
+
+
+        <button className="navbar-toggler d-xl-none navbar__menu" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="34" viewBox="0 0 50 34" fill="none">
+            <path d="M6 2H44" stroke="black" stroke-width="3" />
+            <path d="M0 17H50" stroke="black" stroke-width="3" />
+            <path d="M6 32H44" stroke="black" stroke-width="3" />
+          </svg>
         </button>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+
+        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title navbar__title" id="offcanvasNavbarLabel">{TITLE}</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
-          <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li>
-                    <hr class="dropdown-divider"/>
+
+          <div className="offcanvas-body">
+
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              {
+                BUTTONS.map((button, index) => (
+                  <li className='nav-item' key={`nav-item-${index}`} >
+                    <Link href={button.href} className='nav-link'>
+                      {button.name}
+                    </Link>
                   </li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
+                ))
+              }
             </ul>
-            <form class="d-flex mt-3" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+
           </div>
         </div>
       </div>
